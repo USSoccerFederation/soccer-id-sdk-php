dcde99486725341d3fca24525ca7daa70c6fcce7
Katherine Sterling <elverion@users.noreply.github.com>
2026-06-30 15:20:32 -0300

    Code reorganization & PHPDocs, more secure state/nonce
    Rearranged method order to be group public methods and protected/private methods separately, and to order the public methods in a more "logical" (order they are used) fashion.
    
    Not a big deal, but IMO this helps to more easily follow the code during review, or while maintaining.
    
    Additionally, ensure that each method has at least some basic PHPDOC explaining its purpose and any oddities or quirks it may have.
    
    Finally, made minor adjustments to the state/nonce generation to be more cryptographically secure

--- src/Auth/Auth0Client.php
+++ src/Auth/Auth0Client.php
@@ -54,58 +54,47 @@ class Auth0Client
     }
 
     /**
-     * Log out of the application both locally and on the IdP (will require credentials to log back in)
+     * Initiate the login process by directing the user's browser to the login portal
      *
-     * @param string|null $returnUri
      * @return void
+     * @throws \Random\RandomException
      */
     #[NoReturn]
-    public function logout(?string $returnUri = null): void
+    public function login(): void
     {
-        if ($returnUri === null) {
-            $returnUri = $this->auth0Configuration->redirectUri;
+        $this->logger->debug('Starting Auth0 login', ['session_id' => session_id()]);
+        $this->transientStore->clear();
+        $state = $this->genNewState();
+        $nonce = $this->genNewState();
+        $this->transientStore->set('nonce', $nonce);
+
+        $codeChallenge = null;
+        if ($this->auth0Configuration->usePkce) {
+            // Need to follow PKCE spec; strip disallowed characters
+            $codeVerifier = rtrim(strtr(base64_encode(random_bytes(64)), '+/', '-_'), '=');
+            $codeChallenge = rtrim(strtr(base64_encode(hash('sha256', $codeVerifier, true)), '+/', '-_'), '=');
+            $this->transientStore->set('code_verifier', $codeVerifier);
         }
 
-        if (!(str_starts_with($returnUri, 'http://') || str_starts_with($returnUri, 'https://'))) {
-            $returnUri = (new Path($this->getBaseUrl()))->join($returnUri)->toString();
-        }
-
-        $this->flushStores();
-        $location = $this->getLogoutUri($returnUri);
-        header("Location: {$location}");
+        // Store local data and direct the user to authorization endpoint
+        $this->transientStore->set('state', $state);
+        $this->transientStore->save();
+        $url = $this->getLoginUri($state, $codeChallenge, $nonce);
+        header("Location: {$url}");
         exit();
     }
 
     /**
-     * Return the current session, if logged in, otherwise returns null
+     * Handles the "callback" part of the OAuth2 flow. This is where the user lands after having entered their
+     * credentials on the IdP login portal. If the code exchange succeeds, the user can be considered authenticated.
      *
-     * @return Auth0Session|null
+     * @return Auth0Session
+     * @throws \Random\RandomException
      */
-    public function getSession(): ?Auth0Session
-    {
-        if ($this->auth0Session !== null) {
-            return $this->auth0Session;
-        }
-
-        $storedSession = $this->statefulStore->get('session');
-        if ($storedSession !== null) {
-            $session = Auth0Session::fromStdObject($storedSession);
-            $session->accessTokenExpired = $session->accessTokenExpiration < time();
-
-            if (!$session->accessTokenExpired) {
-                $this->auth0Session = $session;
-            } else {
-                $this->statefulStore->delete('session');
-            }
-        }
-
-        return $this->auth0Session;
-    }
-
     public function callback(): Auth0Session
     {
-        $state = $_GET['state'];
-        $code = $_GET['code'];
+        $state = trim($_GET['state']);
+        $code = trim($_GET['code']);
         $this->logger->debug('Starting Auth0 Callback', [
             'session_id' => session_id(),
             'state' => $state,
@@ -137,123 +126,22 @@ class Auth0Client
         }
     }
 
-    protected function getCallbackRoute(): string
-    {
-        return (new Path($this->getBaseUrl()))->join($this->auth0Configuration->callbackRoute);
-    }
-
-    protected function getBaseUrl(): string
-    {
-        if (!empty($this->auth0Configuration->baseUrl)) {
-            return $this->auth0Configuration->baseUrl;
-        }
-
-        $url = Http::determineHttpHost();
-        if ($url !== null) {
-            return $url;
-        }
-
-        throw new RuntimeException('Unable to determine base URL.');
-    }
-
-    protected function getRedirectUri(): string
-    {
-        $redirectUri = $this->auth0Configuration->redirectUri;
-        if (empty($redirectUri) || $redirectUri === '/') {
-            $redirectUri = (new Path($this->getBaseUrl()))
-                ->join($this->auth0Configuration->callbackRoute)
-                ->toString();
-        }
-
-        return $redirectUri;
-    }
-
-    protected function getAuthBaseUrl(): string
-    {
-        $url = $this->auth0Configuration->domain;
-        if (!str_starts_with($url, 'https://') && !str_starts_with($url, 'http://')) {
-            $url = 'https://' . $url;
-        }
-
-        return $url;
-    }
-
-    protected function getLoginUri(
-        string $state,
-        ?string $codeChallenge = null,
-        ?string $nonce = null,
-    ): string {
-        $redirectUri = $this->getRedirectUri();
-
-        $params = [
-            'response_mode' => 'query',
-            'response_type' => 'code',
-            'scope' => 'openid profile email',
-            'redirect_uri' => $redirectUri,
-            'client_id' => $this->auth0Configuration->clientId,
-            'audience' => $this->auth0Configuration->audience,
-            'state' => $state,
-            'nonce' => $nonce,
-        ];
-
-        if ($codeChallenge !== null) {
-            $params['code_challenge'] = $codeChallenge;
-            $params['code_challenge_method'] = 'S256';
-        }
-
-        $uri = (new Path($this->getAuthBaseUrl()))
-                ->join(static::AUTHORIZE_ENDPOINT)
-                ->toString() . '?' . http_build_query($params);
-
-        return $uri;
-    }
-
-    protected function getLogoutUri(string $returnTo): string
-    {
-        $path = (new Path($this->getAuthBaseUrl()))
-            ->join('v2/logout')
-            ->toString();
-
-        $params = [
-            'returnTo' => $returnTo,
-            'client_id' => $this->auth0Configuration->clientId,
-        ];
-
-        return $path . '?' . http_build_query($params);
-    }
-
-    protected function genNewState(): string
-    {
-        static $inc = 1;
-        $fingerprint = gethostname() . microtime(false);
-        return hash('sha1', $fingerprint . ($inc++) . uniqid(), false);
-    }
-
-    #[NoReturn]
-    public function login(): void
-    {
-        $this->logger->debug('Starting Auth0 login', ['session_id' => session_id()]);
-        $this->transientStore->clear();
-        $state = $this->genNewState();
-        $nonce = hash('sha1', uniqid(more_entropy: true));
-        $this->transientStore->set('nonce', $nonce);
-
-        $codeChallenge = null;
-        if ($this->auth0Configuration->usePkce) {
-            // Need to follow PKCE spec; strip disallowed characters
-            $codeVerifier = rtrim(strtr(base64_encode(random_bytes(64)), '+/', '-_'), '=');
-            $codeChallenge = rtrim(strtr(base64_encode(hash('sha256', $codeVerifier, true)), '+/', '-_'), '=');
-            $this->transientStore->set('code_verifier', $codeVerifier);
-        }
-
-        // Store local data and direct the user to authorization endpoint
-        $this->transientStore->set('state', $state);
-        $this->transientStore->save();
-        $url = $this->getLoginUri($state, $codeChallenge, $nonce);
-        header("Location: {$url}");
-        exit();
-    }
-
+    /**
+     * Performs the code exchange between your application and the IdP, using information gathered from the
+     * user's browser session, to ensure that all parties agree. If any discrepancies or errors occur along
+     * the way, an Exception will be raised, otherwise the user will be authenticated and an `Auth0Session`
+     * returned.
+     * 
+     * @param string $redirectUri
+     * @param string|null $code
+     * @param string|null $state
+     * @return Auth0Session
+     * @throws CodeException
+     * @throws FailedCodeExchangeException
+     * @throws StateException
+     * @throws \JsonException
+     * @throws \Psr\Http\Client\ClientExceptionInterface
+     */
     public function exchange(
         string $redirectUri,
         ?string $code = null,
@@ -360,6 +248,208 @@ class Auth0Client
         return $session;
     }
 
+    /**
+     * Log out of the application both locally and on the IdP (will require credentials to log back in)
+     *
+     * @param string|null $returnUri
+     * @return void
+     */
+    #[NoReturn]
+    public function logout(?string $returnUri = null): void
+    {
+        if ($returnUri === null) {
+            $returnUri = $this->auth0Configuration->redirectUri;
+        }
+
+        if (!(str_starts_with($returnUri, 'http://') || str_starts_with($returnUri, 'https://'))) {
+            $returnUri = (new Path($this->getBaseUrl()))->join($returnUri)->toString();
+        }
+
+        $this->flushStores();
+        $location = $this->getLogoutUri($returnUri);
+        header("Location: {$location}");
+        exit();
+    }
+
+    /**
+     * Return the current session, if logged in, otherwise returns null
+     *
+     * @return Auth0Session|null
+     */
+    public function getSession(): ?Auth0Session
+    {
+        if ($this->auth0Session !== null) {
+            return $this->auth0Session;
+        }
+
+        $storedSession = $this->statefulStore->get('session');
+        if ($storedSession !== null) {
+            $session = Auth0Session::fromStdObject($storedSession);
+            $session->accessTokenExpired = $session->accessTokenExpiration < time();
+
+            if (!$session->accessTokenExpired) {
+                $this->auth0Session = $session;
+            } else {
+                $this->statefulStore->delete('session');
+            }
+        }
+
+        return $this->auth0Session;
+    }
+
+    /**
+     * Shorthand method for purging the user's transient & stateful storage (ex: cookies & session). This is
+     * typically used when an error or data mismatch has occurred during the OAuth2 process and the data
+     * should be cleared prior to restarting the process over.
+     *
+     * This does *not* clear transient or stateful storage on the IdP end; it focuses purely on your application.
+     * @return void
+     */
+    public function flushStores(): void
+    {
+        $this->statefulStore->clear();
+        $this->transientStore->clear();
+        $this->transientStore->save();
+    }
+
+    /**
+     * Get the base URL (schema + domain) of your application
+     * @return string
+     */
+    protected function getBaseUrl(): string
+    {
+        if (!empty($this->auth0Configuration->baseUrl)) {
+            return $this->auth0Configuration->baseUrl;
+        }
+
+        $url = Http::determineHttpHost();
+        if ($url !== null) {
+            return $url;
+        }
+
+        throw new RuntimeException('Unable to determine base URL.');
+    }
+
+    /**
+     * Get the fully-formed callback URI (domain + configured callback route) of your application
+     * @return string
+     */
+    protected function getCallbackRoute(): string
+    {
+        return (new Path($this->getBaseUrl()))->join($this->auth0Configuration->callbackRoute);
+    }
+
+    /**
+     * Get the fully-formed redirect URI of your application - where the user should be sent after logging out.
+     * @return string
+     */
+    protected function getRedirectUri(): string
+    {
+        $redirectUri = $this->auth0Configuration->redirectUri;
+        if (empty($redirectUri) || $redirectUri === '/') {
+            $redirectUri = (new Path($this->getBaseUrl()))
+                ->join($this->auth0Configuration->callbackRoute)
+                ->toString();
+        }
+
+        return $redirectUri;
+    }
+
+    /**
+     * Get the IdP authorization (ex: Auth0 Universal Domain) base URL for U. S. Soccer Federation
+     * @return string
+     */
+    protected function getAuthBaseUrl(): string
+    {
+        $url = $this->auth0Configuration->domain;
+        if (!str_starts_with($url, 'https://') && !str_starts_with($url, 'http://')) {
+            $url = 'https://' . $url;
+        }
+
+        return $url;
+    }
+
+    /**
+     * Get the fully-formed login URI. This is where the user's browser should be directed to in order to
+     * initiate the login process using U. S. Soccer Federation's authorization page.
+     *
+     * @param string $state
+     * @param string|null $codeChallenge
+     * @param string|null $nonce
+     * @return string
+     */
+    protected function getLoginUri(
+        string $state,
+        ?string $codeChallenge = null,
+        ?string $nonce = null,
+    ): string {
+        $redirectUri = $this->getRedirectUri();
+
+        $params = [
+            'response_mode' => 'query',
+            'response_type' => 'code',
+            'scope' => 'openid profile email',
+            'redirect_uri' => $redirectUri,
+            'client_id' => $this->auth0Configuration->clientId,
+            'audience' => $this->auth0Configuration->audience,
+            'state' => $state,
+            'nonce' => $nonce,
+        ];
+
+        if ($codeChallenge !== null) {
+            $params['code_challenge'] = $codeChallenge;
+            $params['code_challenge_method'] = 'S256';
+        }
+
+        $uri = (new Path($this->getAuthBaseUrl()))
+                ->join(static::AUTHORIZE_ENDPOINT)
+                ->toString() . '?' . http_build_query($params);
+
+        return $uri;
+    }
+
+    /**
+     * Get the fully-formed logout URI. This is where the user's browser should be directed to in order
+     * to log the user out on the IdP side. They should then be redirected back to the given `$returnTo`
+     * address on your application.
+     *
+     * @param string $returnTo
+     * @return string
+     */
+    protected function getLogoutUri(string $returnTo): string
+    {
+        $path = (new Path($this->getAuthBaseUrl()))
+            ->join('v2/logout')
+            ->toString();
+
+        $params = [
+            'returnTo' => $returnTo,
+            'client_id' => $this->auth0Configuration->clientId,
+        ];
+
+        return $path . '?' . http_build_query($params);
+    }
+
+    /**
+     * Generate and return a cryptographically-secure random string that is suitable for usage as an
+     * OAuth2 `state` or `nonce`.
+     *
+     * @return string
+     */
+    protected function genNewState(): string
+    {
+        return bin2hex(random_bytes(32));
+    }
+
+    /**
+     * Given a valid JWT, this will extract the claims from the `payload` section and return them as
+     * an array of key-value pairs.
+     *
+     * This does *not* handle JWT verification or validation.
+     * @param string $token
+     * @return array
+     * @throws \JsonException
+     */
     protected function extractTokenClaims(string $token): array
     {
         $parts = explode('.', $token);
@@ -370,12 +460,5 @@ class Auth0Client
 
         $decoded = base64_decode($parts[1]);
         return json_decode($decoded, true, flags: JSON_THROW_ON_ERROR);
-    }
-
-    public function flushStores(): void
-    {
-        $this->statefulStore->clear();
-        $this->transientStore->clear();
-        $this->transientStore->save();
     }
 }
