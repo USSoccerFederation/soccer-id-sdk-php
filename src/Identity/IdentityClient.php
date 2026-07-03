9cb69adec39f7076e3a12a020051043727b35021
Katherine Sterling <elverion@users.noreply.github.com>
2026-07-03 17:16:11 -0300

    Don't log raw body

--- src/Identity/IdentityClient.php
+++ src/Identity/IdentityClient.php
@@ -56,7 +56,6 @@ class IdentityClient
         }
 
         $body = $response->getBody()->getContents();
-        $this->logger->debug("Raw body: {$body}");
         try {
             $decoded = json_decode(
                 json: $body,
