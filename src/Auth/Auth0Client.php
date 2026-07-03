1f9e415205f29c775d8f57435c357122d3f92150
Katherine Sterling <elverion@users.noreply.github.com>
2026-07-03 17:02:30 -0300

    Cleanup debug log

--- src/Auth/Auth0Client.php
+++ src/Auth/Auth0Client.php
@@ -517,12 +517,7 @@ class Auth0Client
         if ($remainder > 0) { // required length to be a multiple of 4
             $payload .= str_repeat('=', 4 - $remainder);
         }
-
-        $this->logger->debug('extracting claims', [
-            'token' => $token,
-            'payload' => $payload,
-            'remainder' => $remainder,
-        ]);
+        
         $decoded = base64_decode($payload, true);
         if ($decoded === false) {
             throw new Exception('Payload contains invalid Base64 characters');
